1e50be26bafa67aad812cb6f6392f1a80d918796
Caglar Yalcin <github@caglaryalcin.com>
2023-03-16 10:07:17 +0300

    updated rotating text

--- src/components/snippet-card/snippet-card.component.jsx
+++ src/components/snippet-card/snippet-card.component.jsx
@@ -25,7 +25,7 @@ const SnippetCard = () => {
 				<Snippet>System Engineer at <CustomLink targetUrl='https://intertech.com.tr'>InterTech</CustomLink></Snippet>
 				<Description>
 				I'm an experienced System Administrator in the field virtualization and interested in open source solutions, computer hardware and professional IT infrastructure products.<br />
-				I <b><ReactRotatingText items={['love free and open-source software (FOSS).', 'love privacy.', 'love security.', 'love virtualization platforms.', 'love writing scripts.','also hate Facebook platforms and would never recommend using it. Take important of your privacy.']} /></b>
+				I <b><ReactRotatingText items={['love free and open-source software (FOSS).', 'love privacy.', 'love security.', 'love virtualization platforms.', 'love writing scripts.','love Open Systems Interconnection (OSI).']} /></b>
 				</Description>
 				<SocialIcons>
 					<CustomIcon

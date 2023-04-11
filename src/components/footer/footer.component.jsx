8e21b95dc277d290e1f37c2acb48c9d754f4478b
Caglar Yalcin <github@caglaryalcin.com>
2023-04-11 23:29:21 +0300

    added pgp

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-
-import { FooterDiv } from './footer.styles'
-
-import CustomEmoji from './../custom-emoji/custom-emoji.component'
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { FooterDiv } from "./footer.styles";
+import { HiOutlineKey } from "react-icons/hi";
 
 const Footer = () => {
-    return (
-        <>
-            <FooterDiv>
-                © {new Date().getFullYear() + " "}
-                Caglar Yalcin
-                <br /><a href="mailto:mail@caglaryalcin.com">mail[at]caglaryalcin.com</a> <CustomEmoji label='mailbox' emoji='📩' />
-            </FooterDiv>
-        </>
-    )
-}
+  return (
+    <>
+      <FooterDiv>
+        © {new Date().getFullYear() + " "}
+        Caglar Yalcin
+        <br />
+        <a href="mailto:mail@caglaryalcin.com">mail[at]caglaryalcin.com </a>
+        <AiOutlineMail /> &nbsp;
+        <a href="https://raw.githubusercontent.com/caglaryalcin/pgp-public-key/main/public.asc" target="_blank">
+          <HiOutlineKey />
+        </a>
+      </FooterDiv>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;

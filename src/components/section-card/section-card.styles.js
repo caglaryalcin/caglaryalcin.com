7e0b72aa3c67eae05f1abb98eee3cc6b1ca749b9
Çağlar Yalçın <github@caglaryalcin.com>
2023-12-09 00:47:00 +0300

    css changed

--- src/components/section-card/section-card.styles.js
+++ src/components/section-card/section-card.styles.js
@@ -1,89 +1,122 @@
-import styled from 'styled-components'
-import 'typeface-ubuntu'
+import styled, { keyframes } from "styled-components";
+import "typeface-ubuntu";
+
+const blinkCursor = keyframes`
+    0%, 100% { opacity: 1; }
+    50% { opacity: 0; }
+`;
+
+export const BlinkingCursor = styled.span`
+  animation: ${blinkCursor} 0.9s steps(1, end) infinite;
+  font-weight: 2;
+  transform: scaleX(0.8);
+  font-size: 15px;
+`;
+
+const PSblinkCursor = keyframes`
+    0%, 100% { opacity: 1; }
+    50% { opacity: 0; }
+`;
+
+export const PSBlinkingCursor = styled.span`
+  animation: ${PSblinkCursor} 1s steps(1, end) infinite;
+  display: inline-block;
+  font-size: 9px;
+  transform: rotate(90deg);
+  vertical-align: -3px;
+`;
 
 export const SectionTitle = styled.div`
-    font-weight: bold;
-    font-size: 24px;
-    margin-bottom: 20px;
-    margin-top: 40px;
-    padding: 10px 20px;
-    border-bottom: 4px solid #413f3f;
-    @media (max-width: 768px) {
-        font-size: 22px;
-    }
-`
+  font-weight: bold;
+  font-size: 24px;
+  margin-bottom: 20px;
+  margin-top: 40px;
+  padding: 10px 20px;
+  border-bottom: 4px solid #413f3f;
+  @media (max-width: 768px) {
+    font-size: 22px;
+  }
+`;
 
 export const SubTitle = styled.span`
-    font-weight: bold;
-`
+  font-weight: bold;
+`;
 
 export const ItemList = styled.ul`
-    margin-top: 5px;
-    list-style: none;
-    padding-left: 0;
-    margin-left: 0;
-`
+  margin-top: 5px;
+  list-style: none;
+  padding-left: 0;
+  margin-left: 0;
+`;
 
 export const Edulist = styled.ul`
-    margin-top: 15px;
-    list-style: none;
-    padding-left: 0;
-    margin-left: 0;
-`
+  margin-top: 15px;
+  list-style: none;
+  padding-left: 0;
+  margin-left: 0;
+`;
 
 export const BoldText = styled.span`
-    font-weight: bold;
-`
+  font-weight: bold;
+`;
 
 export const StackList = styled.ul`
-    margin-top: 5px;
-    list-style: square inside;
-    padding-left: 0;
-    margin-left: 0;
-    line-height: 1.8;
-`
+  margin-top: 5px;
+  list-style: square inside;
+  padding-left: 0;
+  margin-left: 0;
+  line-height: 1.8;
+`;
 
-export const Item = styled.li`
-`
+export const Item = styled.li``;
 
 export const Date = styled.em`
-    color: #888;
-    font-size: 16px;
-`
+  color: #888;
+  font-size: 16px;
+`;
 
 export const ItalicText = styled.span`
-    font-style: italic;
-`
+  font-style: italic;
+`;
 
 export const Tech = styled.span`
-    font-weight: bold;
-    font-size: 17px;
-    border-radius: 3px;
-    padding: 2px 7px;
-`
+  font-weight: bold;
+  font-size: 17px;
+  border-radius: 3px;
+  padding: 2px 7px;
+`;
 
 export const Bash = styled.span`
-    background-color: #300a24;
-    color: #7dd033;
-    font-family: "Ubuntu", "Cascadia Code";
-    font-size: 17px;
-    border-radius: 3px;
-    padding: 2px 7px;
-`
+  background-color: #300a24;
+  color: #7dd033;
+  font-family: "Ubuntu", "Cascadia Code";
+  font-size: 17px;
+  border-radius: 3px;
+  padding-right: -5px;
+  padding-bottom: 2px;
+`;
 export const Bash2 = styled.span`
-    background-color: #300a24;
-    color: #d8d6d0;
-    font-family: "Ubuntu", "Cascadia Code";
-    font-size: 17px;
-    border-radius: 3px;
-    padding: 2px 5px;
-`
+  color: #d8d6d0;
+  font-family: "Ubuntu", "Cascadia Code";
+  font-size: 17px;
+  border-radius: 3px;
+  padding: 2px;
+`;
+
+export const Bash3 = styled.span`
+  color: #6d97c5;
+  font-family: "Ubuntu", "Cascadia Code";
+  font-size: 17px;
+  border-radius: 3px;
+  padding: 2px;
+`;
 
 export const PsComponent = styled.span`
-    background-color: #012456;
-    color: #e2dfd7;
-    font-family: "Lucida Console";
-    font-size: 17px;
-    border-radius: 3px;
-    padding: 3px 7px;
-`
+  background-color: #012456;
+  color: #e2dfd7;
+  font-family: "Consolas", monospace;
+  font-size: 17px;
+  border-radius: 3px;
+  padding-right: 5px;
+  padding-bottom: 1px;
+`;
--- src/components/skills-card/skills-card.component.jsx
+++ src/components/skills-card/skills-card.component.jsx
@@ -1,36 +1,65 @@
-import React from 'react'
+import React from "react";
+import {
+  BlinkingCursor,
+  PSBlinkingCursor,
+} from "./../section-card/section-card.styles";
 
 import {
-	SectionTitle,
-	StackList,
-	Item,
-	Tech,
-	Bash,
-	Bash2,
-	PsComponent,
-} from './../section-card/section-card.styles'
-import ReactRotatingText from 'react-rotating-text';
+  SectionTitle,
+  StackList,
+  Item,
+  Tech,
+  Bash,
+  Bash2,
+  Bash3,
+  PsComponent,
+} from "./../section-card/section-card.styles";
 
 const SkillsCard = () => {
-	return (
-		<>
-			<SectionTitle>Technology Stack</SectionTitle>
-			<StackList>
-				<Item>
-					I'm currently focused on<Tech style={{ borderBottom: '3px solid #0db7ed' }}>Docker</Tech>platform,<Tech style={{ borderBottom: '3px solid #ff9900' }}>AWS</Tech> services and<Tech style={{ borderBottom: '3px solid #62dafb' }}>Next.js</Tech>.
-				</Item>
-				<Item>
-					I'm using <Tech style={{ borderBottom: '3px solid #cf4f4f' }}>Linux distributions</Tech>for both desktop and server, and I greatly enjoy using them.
-				</Item>
-				<Item>
-					I'm familiar with<Tech style={{ borderBottom: '3px solid #fc6d26' }}>Gitlab CI/CD</Tech>.
-				</Item>
-				<Item>
-					I'm writing scripts in <Bash style={{ borderBottom: '3px solid #300a24' }}>user@ubuntu<Bash2>:~$<ReactRotatingText items={['']} /></Bash2></Bash> and <PsComponent style={{ borderBottom: '3px solid #012456' }}>PS C:\> <ReactRotatingText items={['']} /></PsComponent> based on the specific needs of the institutions I work with.
-				</Item>
-			</StackList>
-		</>
-	)
-}
+  return (
+    <>
+      <SectionTitle>Technology Stack</SectionTitle>
+      <StackList>
+        <Item>
+          I'm currently focused on
+          <Tech style={{ borderBottom: "3px solid #0db7ed" }}>Docker</Tech>
+          platform,
+          <Tech style={{ borderBottom: "3px solid #ff9900" }}>AWS</Tech>{" "}
+          services and
+          <Tech style={{ borderBottom: "3px solid #62dafb" }}>Next.js</Tech>.
+        </Item>
+        <Item>
+          I'm using{" "}
+          <Tech style={{ borderBottom: "3px solid #cf4f4f" }}>
+            Linux distributions
+          </Tech>
+          for both desktop and server, and i greatly enjoy using them.
+        </Item>
+        <Item>
+          I'm familiar with
+          <Tech style={{ borderBottom: "3px solid #fc6d26" }}>
+            Gitlab CI/CD
+          </Tech>
+          .
+        </Item>
+        <Item>
+          I'm writing scripts in{" "}
+          <Bash style={{ borderBottom: "3px solid #300a24" }}>
+            bash@linux
+            <Bash2>
+              :<Bash3>~</Bash3>$ <span style={{ whiteSpace: "pre" }}> </span>
+              <BlinkingCursor>█</BlinkingCursor>
+            </Bash2>
+          </Bash>{" "}
+          and{" "}
+          <PsComponent style={{ borderBottom: "3px solid #012456" }}>
+            PS C:\> <PSBlinkingCursor>█</PSBlinkingCursor>
+          </PsComponent>{" "}
+          based on the specific needs of the institutions i work with.
+        </Item>
+      </StackList>
+    </>
+  );
+};
 
-export default SkillsCard
+export default SkillsCard;

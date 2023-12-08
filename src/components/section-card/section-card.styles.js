26afd4910ed89887207ab70b4c8b5963c82dd417
Çağlar Yalçın <github@caglaryalcin.com>
2023-12-08 18:46:28 +0300

    added ubuntu font

--- src/components/section-card/section-card.styles.js
+++ src/components/section-card/section-card.styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import 'typeface-ubuntu'
 
 export const SectionTitle = styled.div`
     font-weight: bold;
@@ -64,7 +65,7 @@ export const Tech = styled.span`
 export const Bash = styled.span`
     background-color: #300a24;
     color: #7dd033;
-    font-family: "Cascadia Code";
+    font-family: "Ubuntu", "Cascadia Code";
     font-size: 17px;
     border-radius: 3px;
     padding: 2px 7px;
@@ -72,7 +73,7 @@ export const Bash = styled.span`
 export const Bash2 = styled.span`
     background-color: #300a24;
     color: #d8d6d0;
-    font-family: "Cascadia Code";
+    font-family: "Ubuntu", "Cascadia Code";
     font-size: 17px;
     border-radius: 3px;
     padding: 2px 5px;
